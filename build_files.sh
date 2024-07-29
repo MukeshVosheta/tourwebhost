033f374c0c0f7125dfda59f3b5980b602fb1dac2
MukeshVosheta <sunkon.com@gmail.com>
2024-07-29 12:36:12 +0530

    bs

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,9 @@
-echo "Python version:"
-python3.9 --version
-echo "Pip version:"
-python3.9 -m pip --version
-echo "Installing requirements:"
+# Install pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
+
+# Install the requirements
 python3.9 -m pip install -r requirements.txt
-echo "Collecting static files:"
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput --clear
