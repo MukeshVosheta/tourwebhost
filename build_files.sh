ca9e67d1c47fe5328300d027a377a1513c3de7b3
MukeshVosheta <sunkon.com@gmail.com>
2024-07-29 12:34:19 +0530

    bs

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
-pip install -r requirements.txt
+echo "Python version:"
+python3.9 --version
+echo "Pip version:"
+python3.9 -m pip --version
+echo "Installing requirements:"
+python3.9 -m pip install -r requirements.txt
+echo "Collecting static files:"
 python3.9 manage.py collectstatic --noinput --clear
